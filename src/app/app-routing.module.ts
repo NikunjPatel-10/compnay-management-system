e4f02b3f24a852884d4fb137d02622836847f72b
Nikunj Patel <nikunj.patel@1Rintern.local>
2022-10-12 15:01:06 +0530

    feat:routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) }];
+const routes: Routes = [{ path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) },
+{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'company'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
